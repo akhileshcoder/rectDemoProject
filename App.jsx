5e8002468e0be3bd5da9d389ebe1c49658b52b0f
akhilesh Kumar <akhilesh.kumar@gmail.com>
2017-01-24 09:01:56 +0530

    reactLact

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import React from "react";// var React =require("react")
+
+class App extends React.Component{
+    render(){
+        return (
+            <div>
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default App;
